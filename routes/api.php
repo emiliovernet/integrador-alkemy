546197885924f2abfe3c8d010e541fdf8161f14b
emiliovernet <vernetemilio@gmail.com>
2023-10-24 17:06:31 -0300

    Se crean endpoints y controller carrito de compras y orden de compra

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Http\Controllers\CarritoController;
+use App\Http\Controllers\OrdenesController;
 use App\Http\Controllers\PruebaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,8 +20,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/saludo', [PruebaController::class, 'saludar']
-);
+// Route::get('/saludo', [PruebaController::class, 'saludar']
+// );
 
-Route::post('/despedida', [PruebaController::class, 'saludar']
-);
+// // Route::post('/despedida', [PruebaController::class, 'saludar']
+// // );
+
+// RUTAS CARRITO DE COMRAS
+
+Route::get('/carrito', [CarritoController::class, 'index']);
+Route::post('/carrito/agregar', [CarritoController::class, 'agregarProducto']);
+Route::put('/carrito/actualizar/{idProducto}', [CarritoController::class, 'actualizarProducto']);
+Route::delete('/carrito/eliminar/{idProducto}', [CarritoController::class, 'eliminarProducto']);
+Route::get('/carrito/listar/{idCarrito}', [CarritoController::class, 'lista']);
+Route::get('/carrito/resumir/{idCarrito}', [CarritoController::class, 'resumen']);
+Route::delete('/carrito/limpiar/{idCarrito]', [CarritoController::class, 'limpiar']);
+Route::get('/carrito/finalizar/{idCarrito}', [CarritoController::class, 'finalizarCarrito']);
+
+
+
+// RUTAS ORDEN DE COMPRA
+
+Route::post('/ordenes/crear', [OrdenesController::class, 'crearOrden']);
+
+
